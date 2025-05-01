1a529b904b25845282090d5455dfb4620fa1f6ff
Gerardo314 <128551623+Gerardo314@users.noreply.github.com>
2025-05-01 03:10:25 -0500

    more comments

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -7,20 +7,31 @@ import java.time.LocalDateTime;
 import java.util.concurrent.BlockingQueue;
 import java.util.logging.*;
 
+/**
+ * This class represents the handler thread for each new client 
+ */
 public class ClientHandler implements Runnable {
 
     private Socket socket;
     private BlockingQueue<ClientRequest> queue;
 
+    /**
+     * Constructs the clienthandler for each new client 
+     * @param socket the socket where the client/server communicate through
+     * @param queue The main queue that the worker thread uses to evaluate new expressions
+     */
     public ClientHandler(Socket socket, BlockingQueue<ClientRequest> queue){
         this.socket = socket;
         this.queue = queue;
     }
 
+    /**
+     * Runs the client handler thread
+     */
     public void run(){
-        Logger logger = Logger.getLogger("MathServerLogger");
+        Logger logger = Logger.getLogger("MathServerLogger"); //creating a new logger for server to log new clients, new expresisons, and expression results 
          try {
-            // Set up the logger only once (first time)
+            // Set up the logger 
             synchronized (logger) {
                 if (logger.getHandlers().length == 0) {
                     FileHandler handler = new FileHandler("server.log", true);
@@ -49,12 +60,12 @@ public class ClientHandler implements Runnable {
                     logger.info("Client "+socket.getInetAddress()+" Port: "+socket.getPort()+" has been connected at "+LocalDateTime.now());
                     
                 }
-                else if(messageObject.getMessageType().equals("terminate")){
+                else if(messageObject.getMessageType().equals("terminate")){ //ends communication if client requests it
                     logger.info("Client "+socket.getInetAddress()+" Port: "+socket.getPort()+" has been terminated at "+LocalDateTime.now());
                     socket.close();
                     return;
                 }
-                else{
+                else{ //means there is an expression to be solved, puts it in the main queue for worker thread 
                     queue.put(new ClientRequest(messageObject,out ));
                     logger.info("Client "+socket.getInetAddress()+" Port: "+socket.getPort()+" sent message: "+messageObject.getMessageText());
                 }
