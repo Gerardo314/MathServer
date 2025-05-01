9c22cbecbabb0af2be3f64f68fb95b5f034880e0
Gerardo314 <128551623+Gerardo314@users.noreply.github.com>
2025-05-01 03:10:59 -0500

    more comments

--- src/ClientRequest.java
+++ src/ClientRequest.java
@@ -1,19 +1,36 @@
-
 import java.io.ObjectOutputStream;
 
+/**
+ * This class represents a request to the Main Server to handle 
+ * The request contains a message that contains the math expression to  be solved
+ * The request also contains an output stream for the result of the expression to be printed to. 
+ */
 public class ClientRequest {
     private Message message;
     private ObjectOutputStream out; // where result of the server will be attached to 
 
+    /**
+     * Constructs the Client Request Class
+     * @param message The message the client sent which is to be handled by the main server's worker thread
+     * @param out the output stream corresponding to the client where the result of the expression will be written to
+     */
     ClientRequest( Message message, ObjectOutputStream out) {
         this.message = message;
         this.out = out;
     }
 
+    /**
+     * Gets the message from the ClientRequest object
+     * @return The message from the ClientRequest
+     */
     public Message getMessage(){
         return message;
     }
 
+    /**
+     * The client's corresponding output stream 
+     * @return the Client's output stream 
+     */
     public ObjectOutputStream getOutput(){
         return out;
     } 
