23b3ea8c4700af93097b2d275766a8632ee8a6f7
Gerardo314 <128551623+Gerardo314@users.noreply.github.com>
2025-05-01 03:11:55 -0500

    more comments

--- src/Message.java
+++ src/Message.java
@@ -1,11 +1,20 @@
 import java.io.Serializable;
 
+/**
+ * This class represents the message object that the clients and server will use to communicate 
+ */
 public class Message implements Serializable
 {
     private String messageType;//connect, connected, expression, result, error, terminate
     private String messageText;//expression Strings, results, error
     private String clientName;//name of the user sending the math expression
 
+    /**
+     * Instantiates the message object 
+     * @param messageType The type of message (expression, connect, terminate)
+     * @param messageText The content of the message 
+     * @param clientName The name of the sender 
+     */
     public Message(String messageType,String messageText,String clientName)
     {
         this.messageType = messageType; 
@@ -14,32 +23,57 @@ public class Message implements Serializable
     }
 
     //define getters methods
+    /**
+     * Gets the type of the message 
+     * @return The message type 
+     */
     public String getMessageType()
     {
         return messageType;
     }
 
+    /**
+     * gets the text in the message 
+     * @return the text in the message 
+     */
     public String getMessageText()
     {
         return messageText;
     }
 
+    /**
+     * Gets the name of the client sending the message
+     * @return
+     */
     public String getClientName()
     {
         return clientName;
     }
 
     //define setters methods
+
+    /**
+     * sets the type of the message
+     * @param messageType the message type
+     */
     public void setMessageType(String messageType)
     {
         this.messageType = messageType;
     }
 
+    /**
+     * Sets the text of the message 
+     * @param messageText the message text 
+     */
     public void setMessageText(String messageText)
     {
         this.messageText = messageText;
     }
 
+    /**
+     * stes the client name of the message 
+     * @param clientName the clientname 
+     */
     public void setClientName(String clientName)
     {
         this.clientName = clientName;
